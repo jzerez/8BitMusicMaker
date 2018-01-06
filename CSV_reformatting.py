791c8401b3775c6f642d3715b8b15cd23367e1bc
jzerez <Jonathan.zerez@students.olin.edu>
2018-01-05 23:02:42 -1000

    trying sequential formatting
    For some reason the 'On' events aren't being tracked. Potentially lots of debugging needs to be done.

--- CSV_reformatting.py
+++ CSV_reformatting.py
@@ -11,114 +11,84 @@ def output_row(row):
 
     return string
 
+#offset to put the unicode characters where i want them
+unicode_offset = 192
 
 #look at each csv in the training folder
 for files in os.listdir("Training-CSV"):
-    num_tracks = 0
-    #input csv
     csv_file = open("Training-CSV/" + files)
     #output csv. Should change to one huge txt file
-    output = open("input.txt", 'a')
-
+    output = open("input.txt", 'w', encoding = 'utf8')
     csv_py = csv.reader(csv_file)
 
-    #valid shows whether an instrument is valid or not
-    valid = False
-    channel = -1
+    array = []
+    notes = {}
+
+    for pitch in range(128):
+        notes[pitch] = False
+
     for row in csv_py:
-        if row[2] == ' Header':
-            #convert ticks per quater note to 120
-            time_conversion_head = 120 / int(row[5])
+        if row[2] == ' Note_on_c' and int(row[5]) > 0:
+            array.append([int(row[1]), 'on', int(row[4]), False])
+            #print(row[4] + ' On!')
+        if row[2] == ' Note_on_c' and int(row[5]) == 0:
+            array.append([int(row[1]), 'off', int(row[4]), False])
+            #print(row[4] + ' Off!')
+        if row[2] == ' Note_off_c':
+            array.append([int(row[1]), 'off', int(row[4]), False])
+            #print(row[4] + ' Off!')
         if row[2] == ' Tempo':
-            #convert the time to use 100 bpm
-            time_conversion_tempo = int(row[3]) / 600000
-
-        if (row[2] == ' Program_c'):
-            #check and consolodate instruments based on groupings from wikipedia
-            instrument = int(row[4])
-
-            valid = True
-            if instrument < 9:
-                row[4] = '5'
-                channel = 0
-            elif instrument < 17:
-                row[4] = '12'
-                channel = 1
-            elif instrument < 25:
-                row[4] = '18'
-                channel = 2
-            elif instrument < 33:
-                row[4] = '28'
-                channel = 3
-            elif instrument < 41:
-                row[4] = '39'
-                channel = 4
-            elif instrument < 49:
-                row[4] = '41'
-                channel = 5
-            elif instrument < 57:
-                row[4] = '56'
-                channel = 6
-            elif instrument < 65:
-                row[4] = '63'
-                channel = 7
-            elif instrument < 73:
-                row[4] = '66'
-                channel = 8
-            elif instrument < 81:
-                row[4] = '80'
-                channel = 9
-            elif instrument < 89:
-                row[4] = '81'
-                channel = 10
-            elif instrument < 97:
-                row[4] = '90'
-                channel = 11
-            elif instrument < 105:
-                row[4] = '99'
-                channel = 12
-            elif instrument < 113:
-                valid = False
-                channel = -1
-            elif instrument < 121:
-                row[4] = '119'
-                channel = 13
-            else:
-                valid = False
-                channel = -1
-
-            #Need to do: write out to the txt or csv file
-            if valid:
-                num_tracks += 1
-                row[0] = str(1 + num_tracks)
-                row[1] = str(hex(int((int(row[1]) * time_conversion_tempo * time_conversion_head))).split('x')[-1])
-                row[2] = 'Prog'
-                row[3] = str(channel)
-                row[4] = str(int(row[4]))
-                output.write(output_row(row))
-
-        #Need to do: write out to txt file
-        if(row[2] == ' Note_on_c' and valid):
-            row[0] = str(1 + num_tracks)
-            row[1] = str(hex(int((int(row[1]) * time_conversion_tempo * time_conversion_head))).split('x')[-1])
-            row[2] = 'On'
-            row[3] = str(channel)
-            row[4] = str(int(row[4]))
-            row[5] = str(int(row[5]))
-            output.write(output_row(row))
+            tempo = int(row[3])
+        if row[2] == ' Header':
+            pulses = int(row[5])
+    array = sorted(array, key = lambda line: line[1])
 
 
-        if(row[2] == ' Note_off_c' and valid):
-            row[0] = str(1 + num_tracks)
-            row[1] = str(hex(int((int(row[1]) * time_conversion_tempo * time_conversion_head))).split('x')[-1])
-            row[2] = 'Of'
-            row[3] = str(channel)
-            row[4] = str(int(row[4]))
-            row[5] = str(int(row[5]))
-            output.write(output_row(row))
-            #writer.writerow(row)
-    print(str(files) + " complete!")
+    step = (pulses / (tempo / 1000000)) / 1000
+    index = 0
+    time = 0
+
+    print(step)
+
+    #iterate the correct number of timestep times
+    #for time_step in range(round(int(array[-1][0]) / step) + 1):
+    for time_step in range(10000):
+        if array[index][0] <= time:
+            dupes = 1
+            try:
+                while array[index][0] == array[index + dupes][0]:
+                    dupes += 1
+            except:
+                print('last')
+                pass
+            #if the current element has not been visited and is less than the current time
+            print(array[index])
+            for i in range(dupes):
+                if not array[index + i][3]:
+
+                    if array[index + i][1] == 'on':
+                        notes[array[index + i][2]] = True
+                        array[index + i][3] = True
+                        #print(str(array[index + i]) + 'ONNNNNNNNNNNNNNNNNNNNNNNNN')
+                    else:
+                        notes[array[index + i][2]] = False
+                        array[index + i][3] = True
+                        #print(str(array[index + i]) + 'OFF')
+            #check to see the next time value to know whether to update time or not
+            index += dupes
+        for note in notes:
+            if notes[note]:
+                char = unicode_offset + note
+                output.write(chr(char))
+        output.write('\n')
+        time += step
+
+
+
     output.write("NEWSONG\n")
+    print(str(files) + " complete!")
+    break
+
 
 
     '''
